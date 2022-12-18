257599e5e7cfa1def00c0bb30b1402f909074d67
Abaevsk94 <abaevsk@gmail.com>
2022-12-18 16:18:08 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,103 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int salary = 15_000;
+        int total = 0;
+        int i = 1;
+        while (total < 2_459_000) {
+            total = total + salary;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + "рублей");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int population = 12_000_000;
+        int fertility = 17 * population / 1000;
+        int mortality = 8 * population / 1000;
+        for (int i = 1; i <= 10; i++) {
+            population = population + fertility;
+            population = population - mortality;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int sum = 15_000;
+        int month = 0;
+        while (sum <= 12_000_000) {
+            month++;
+            sum *= 1.07;
+            System.out.printf("Месяц %d - накопил %d \n", month, sum);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int sum = 15_000;
+        for (int i = 1; sum <= 12_000_000; i++) {
+            sum *= 1.07;
+            if (i % 6 == 0) {
+                System.out.printf("Месяц %d - накопил %d \n", i, sum);
+            }
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        int sum = 15_000;
+        for (int i = 1; i < 12 * 9; i += 6) {
+            int sumBefore = sum;
+            sum *= 1 + 0.07 * 6;
+            System.out.printf("Месяц %d - накопил %d \n", i, sum - sumBefore);
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int day = 2; day <= 31; day = day + 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
+        }
+
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int currentYear = 2022;
+        int minYear = currentYear - 200;
+        int maxYear = currentYear + 100;
+        for (int i = minYear; i <= maxYear; ++i) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
